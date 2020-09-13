04fc7dd258bcbb8740b5bfdb8bcca62ce2a7b210
Kai Hendry <hendry@iki.fi>
2020-09-13 09:14:47 +0800

    Reproduce https://github.com/aws/aws-sdk-go-v2/issues/715 in an AWS SAM environment

--- hello-world/go.mod
+++ hello-world/go.mod
@@ -1,4 +1,8 @@
-require github.com/aws/aws-lambda-go v1.13.3
+require (
+	github.com/apex/log v1.9.0
+	github.com/aws/aws-lambda-go v1.13.3
+	github.com/aws/aws-sdk-go-v2 v0.24.0
+)
 
 module hello-world
 
--- hello-world/main.go
+++ hello-world/main.go
@@ -1,17 +1,47 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/apex/log"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/aws/external"
+	"github.com/aws/aws-sdk-go-v2/service/sns"
 )
 
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+
+	// Doesn't work
+	// cfg, err := external.LoadDefaultAWSConfig(external.WithSharedConfigProfile("mine"))
+	// Works
+	cfg, err := external.LoadDefaultAWSConfig()
+	if err != nil {
+		log.WithError(err).Fatal("setting up credentials")
+	}
+	cfg.Region = "ap-southeast-1"
+
+	client := sns.New(cfg)
+	req := client.PublishRequest(&sns.PublishInput{
+		TopicArn: aws.String(os.Getenv("SNS_TOPIC_ARN")),
+		Message:  aws.String(fmt.Sprintf("time: %s", time.Now())),
+	})
+
+	_, err = req.Send(context.Background())
+	if err != nil {
+		log.WithError(err).Fatal("unable to send SNS")
+	}
+
 	return events.APIGatewayProxyResponse{
 		StatusCode: 200,
 		Headers: map[string]string{
 			"Content-Type": "text/plain",
 		},
-		Body: "Hello Worldo",
+		Body: "Hello World2",
 	}, nil
 }
 
