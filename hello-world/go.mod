751b31b0bffe12b5560611103dd66d5ac4eaf462
Kai Hendry <hendry@iki.fi>
2020-09-13 08:55:27 +0800

    {"message":"Missing Authentication Token"} on POST

--- hello-world/go.mod
+++ hello-world/go.mod
@@ -1,4 +1,12 @@
-require github.com/aws/aws-lambda-go v1.13.3
+require (
+	github.com/apex/gateway v1.1.1
+	github.com/apex/log v1.9.0
+	github.com/aws/aws-lambda-go v1.13.3
+	github.com/gorilla/context v1.1.1 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/gorilla/pat v1.0.1
+	github.com/pkg/errors v0.9.1 // indirect
+)
 
 module hello-world
 
--- hello-world/main.go
+++ hello-world/main.go
@@ -1,20 +1,72 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+
+	"github.com/apex/gateway"
+	"github.com/apex/log"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/gorilla/pat"
 )
 
-func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+// pets database-ish
+var pets = make(map[string]struct{})
+
+func handler(lambdaRequest events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	fmt.Println("got request", lambdaRequest)
+	request, err := gateway.NewRequest(context.Background(), lambdaRequest)
+	if err != nil {
+		fmt.Println("newRequest error", err)
+	}
+
+	app := pat.New()
+	app.Get("/", get)
+	app.Post("/", post)
+
+	res := httptest.NewRecorder()
+	app.ServeHTTP(res, request)
+
 	return events.APIGatewayProxyResponse{
-		StatusCode: 200,
-		Headers: map[string]string{
-			"Content-Type": "text/plain",
-		},
-		Body: "Hello Worldo",
+		Body:       res.Body.String(),
+		StatusCode: res.Code,
 	}, nil
 }
 
 func main() {
 	lambda.Start(handler)
+}
+
+// curl http://localhost:3000/
+func get(w http.ResponseWriter, r *http.Request) {
+	log.Info("list pets")
+
+	if len(pets) == 0 {
+		fmt.Fprintf(w, "no pets")
+		return
+	}
+
+	for name := range pets {
+		fmt.Fprintf(w, "- %s\n", name)
+	}
+}
+
+// curl -d Tobi http://localhost:3000/
+// curl -d Loki http://localhost:3000/
+// curl -d Jane http://localhost:3000/
+func post(w http.ResponseWriter, r *http.Request) {
+	b, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		return
+	}
+
+	name := string(b)
+	pets[name] = struct{}{}
+	log.WithField("name", name).Info("add pet")
+	fmt.Fprintf(w, "welcome to the family %s!\n", name)
 }
